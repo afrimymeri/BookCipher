3b6c6d7a89ffe31da18ccff2d9349ef012d80577
afrimymeri <afrimymeri0@gmail.com>
2024-04-18 15:19:46 +0200

    Load text document function added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+    private static Map<String, Integer> wordIndexMap = new HashMap<>();
     private static void loadTextDocument(String filename) {
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
@@ -10,10 +13,12 @@ public class Main {
             while ((line = br.readLine()) != null) {
                 String[] words = line.split("\\s+");
                 for (String word: words) {
+                    wordIndexMap.put(word.toUpperCase(), index++);
                 }
             }
         }catch (IOException e) {
-
+            System.out.println("Error reading file: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
